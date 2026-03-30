b46b6270a6d6b528bb4dfb0402f3106759c54873
Tiago Vargas <tiagovargas@ymail.com>
2026-03-30 12:29:47 -0300

    feat: implement SocialPost component and update App.vue to use it

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
+<script setup>
+import SocialPost from './components/molecules/SocialPost.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <SocialPost></SocialPost>
 </template>
 
-<style scoped></style>
+<style></style>
