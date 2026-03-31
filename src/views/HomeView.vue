ecb5e6f8c9b016adbe82243292f81f5b0305e216
Tiago Vargas <tiagovargas@ymail.com>
2026-03-31 12:33:13 -0300

    feat: add AboutView and HomeView components with basic structure and styles

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import SideBar from '../components/organisms/SideBar.vue';
+import TheHeader from '../components/organisms/TheHeader.vue';
+</script>
+
+<template>
+  <TheHeader />
+  <SideBar />
+</template>
+
+<style>
+@import '../assets/base.css';
+aside {
+  border-right: 1px solid var(--color-border);
+  padding: 16px 32px;
+}
+</style>
