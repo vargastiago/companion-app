deedfd574dd6c49ccb485533731ae08b4ff75ea9
Tiago Vargas <tiagovargas@ymail.com>
2026-03-31 12:45:01 -0300

    refactor: remove SocialPost and TheHeader components, simplify App.vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,9 @@
 <script setup>
-import { reactive } from 'vue';
-import SocialPost from './components/molecules/SocialPost.vue';
-import TheHeader from './components/organisms/TheHeader.vue';
-const posts = reactive([
-  {
-    username: 'Username one',
-    userId: 'usernameID1',
-    avatarSrc: 'https://i.pravatar.cc/40',
-    post: 'This is my post',
-    comments: ['great post', 'amazing post'],
-    likes: 2,
-    retweets: 1,
-    tags: ['tag 1'],
-  },
-  {
-    username: 'Username two',
-    userId: 'usernameID2',
-    avatarSrc: 'https://i.pravatar.cc/40',
-    post: 'This is my second post',
-    comments: [],
-    likes: 3,
-    retweets: 1,
-    tags: ['tag 1', 'tag 2'],
-  },
-]);
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <TheHeader />
-  <SocialPost
-    v-for="post in posts"
-    :username="post.username"
-    :user-id="post.userId"
-    :avatar-src="post.avatarSrc"
-    :post="post.post"
-    :comments="post.comments"
-    :likes="post.likes"
-    :retweets="post.retweets"
-    :key="post.userId"
-  ></SocialPost>
+  <RouterView />
 </template>
 
-<style></style>
+<style scoped></style>
