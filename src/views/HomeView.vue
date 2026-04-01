acfac5db946e59a90250038254705bd08ccdb9cc
Tiago Vargas <tiagovargas@ymail.com>
2026-03-31 23:00:06 -0300

    feat: add CreatePost component and integrate it into HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CreatePost from '../components/molecules/CreatePost.vue';
 import SideBar from '../components/organisms/SideBar.vue';
 import TheHeader from '../components/organisms/TheHeader.vue';
 </script>
@@ -6,12 +7,18 @@ import TheHeader from '../components/organisms/TheHeader.vue';
 <template>
   <TheHeader />
   <SideBar />
+  <main>
+    <CreatePost />
+  </main>
 </template>
 
 <style>
 @import '../assets/base.css';
 aside {
   border-right: 1px solid var(--color-border);
+  padding: 16px 32px;
+}
+main {
   padding: 16px 32px;
 }
 </style>
