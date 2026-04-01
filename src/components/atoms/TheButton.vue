e1af10719980a8ead55c70572fbd58081a346653
Tiago Vargas <tiagovargas@ymail.com>
2026-04-01 01:39:55 -0300

    feat: add TheButton and TheLogo components for UI enhancement

---
+++ src/components/atoms/TheButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :class="theme">
+    {{ value }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  value: {
+    type: [String, Number],
+    required: true,
+  },
+  width: {
+    type: String,
+    default: '100px',
+  },
+  theme: {
+    type: String,
+    default: 'light',
+    validator: (value) => ['light', 'dark'].includes(value),
+  },
+});
+</script>
+
+<style>
+button {
+  width: v-bind(width);
+}
+.light {
+  background-color: #1da1f2;
+  color: white;
+}
+.dark {
+  background-color: black;
+  color: #1da1f2;
+}
+</style>
