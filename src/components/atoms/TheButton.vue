53d793743b8f8c0d2655a51d02cef1e943919fe4
Tiago Vargas <tiagovargas@ymail.com>
2026-04-01 13:25:42 -0300

    style: enhance button styles in TheButton component

--- src/components/atoms/TheButton.vue
+++ src/components/atoms/TheButton.vue
@@ -25,6 +25,13 @@ defineProps({
 <style>
 button {
   width: v-bind(width);
+  height: 25px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+}
+button:hover {
+  background-color: #156ca1;
 }
 .light {
   background-color: #1da1f2;
@@ -33,5 +40,9 @@ button {
 .dark {
   background-color: black;
   color: #1da1f2;
+  border: 1px solid #1da1f2;
+}
+.dark:hover {
+  color: white;
 }
 </style>
